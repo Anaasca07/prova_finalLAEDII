9d6029a07408b802e5dd61b3e946473713b79532
Tata <112900054+JanieliSilva@users.noreply.github.com>
2024-04-23 20:39:11 -0300

    Update questao4.c

--- questao4.c
+++ questao4.c
@@ -4,16 +4,16 @@
 #define tam 2532
 int matriz[tam][tam];
 
-void sumidouro(int n, int m, int matriz[tam][tam]){
-    int i, aux[n],j;
+void sumidouro(int n, int m, int matriz[][tam]){
+    int i, aux[n], j;
     int temSumidouro = 0;
-    for(i=0;i<n;i++){
+    for(i = 0; i < n; i++){
         aux[i] = 0;
-        for(j=0;j<m;j++){
+        for(j = 0; j < m; j++){
             aux[i] += matriz[i][j];
         }
         if(aux[i] == 0){
-            printf("%d = sumidouro\n",i);
+            printf("%d = sumidouro\n", i);
             temSumidouro = 1;
             return;
         }
@@ -22,7 +22,14 @@ void sumidouro(int n, int m, int matriz[tam][tam]){
 }
 
 int main(){
-    int linha,coluna;
-    sumidouro(linha,coluna, matriz);
-    return ;
+    int linha = tam, coluna = tam;
+    
+    for(int i = 0; i < linha; i++){
+        for(int j = 0; j < coluna; j++){
+            matriz[i][j] = 0;
+        }
+    }
+    
+    sumidouro(linha, coluna, matriz);
+    return 0;
 }
